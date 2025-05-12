b447c17722afe60543f44d5df44f040b47d316bc
NashyMurthy <nashyybusiness@gmail.com>
2025-05-12 18:06:58 +0800

    Update main.tsx
    everything is the same except from App we changed it to AppWrapper, App.tsx to App Wrapper.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import AppWrapper from './AppWrapper.tsx';  // Import the AppWrapper component
 import './index.css';
 
+// Render the AppWrapper component
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppWrapper />  {/* This will render the landing page, auth page, or main app */}
   </StrictMode>
 );
